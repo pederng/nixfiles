aa912820d7d76f06e2471e9ef034ba41a4642a78
Peder Notto Galteland <peder.galteland@softwarelab.no>
2022-06-07 20:45:15 +0200

    move neovim setup to programs.neovim section

--- home.nix
+++ home.nix
@@ -1,10 +1,9 @@
 {
   config,
   pkgs,
+  lib,
   ...
 }: {
-  # Home Manager needs a bit of information about you and the
-  # paths it should manage.
   nixpkgs.overlays = [
     (import (builtins.fetchTarball {
       url = https://github.com/nix-community/neovim-nightly-overlay/archive/master.tar.gz;
@@ -14,36 +13,38 @@
   home.homeDirectory = "/home/peder";
   home.packages = [
     pkgs.tmux
-    pkgs.neovim-nightly
-    pkgs.stylua
-    pkgs.alejandra
-    pkgs.rnix-lsp
-    pkgs.sumneko-lua-language-server
   ];
 
-  # This value determines the Home Manager release that your
-  # configuration is compatible with. This helps avoid breakage
-  # when a new Home Manager release introduces backwards
-  # incompatible changes.
-  #
-  # You can update Home Manager without changing this value. See
-  # the Home Manager release notes for a list of state version
-  # changes in each release.
   home.stateVersion = "21.11";
 
-  # Let Home Manager install and manage itself.
   programs.home-manager.enable = true;
 
-  home.file.".config/nvim/init.lua".source = ./init.lua;
-  home.file.".config/nvim/lua/plugins.lua".source = ./plugins.lua;
-  home.file.".config/nvim/lua/colors.lua".source = ./colors.lua;
-  home.file.".config/nvim/lua/options.lua".source = ./options.lua;
-  home.file.".config/nvim/lua/bindings.lua".source = ./bindings.lua;
-  home.file.".config/nvim/lua/autocmds.lua".source = ./autocmds.lua;
-  home.file.".config/nvim/lua/commands.lua".source = ./commands.lua;
+  programs.neovim = {
+    enable = true;
+    package = pkgs.neovim-nightly;
+    extraConfig = builtins.concatStringsSep "\n" [
+      ''
+        lua << EOF
+        ${lib.strings.fileContents ./plugins.lua}
+        ${lib.strings.fileContents ./colors.lua}
+        ${lib.strings.fileContents ./options.lua}
+        ${lib.strings.fileContents ./bindings.lua}
+        ${lib.strings.fileContents ./autocmds.lua}
+        ${lib.strings.fileContents ./commands.lua}
+        EOF
+      ''
+    ];
+    extraPackages = with pkgs; [
+      stylua
+      alejandra
+      rnix-lsp
+      sumneko-lua-language-server
+    ];
+  };
 
   programs.git = {
     enable = true;
+    package = pkgs.git;
     userName = "Peder Notto Galteland";
     userEmail = "peder.galteland@softwarelab.no";
     aliases = {
