683d0e5e409ad36195e2334ff9f2c3afb9a0bf21
Peder Notto Galteland <peder.galteland@softwarelab.no>
2022-09-23 19:57:49 +0200

    Add terraformls

--- nvim/plugins.lua
+++ nvim/plugins.lua
@@ -143,7 +143,7 @@ local on_attach = function(_, bufnr)
 end
 
 local capabilities = require("cmp_nvim_lsp").update_capabilities(vim.lsp.protocol.make_client_capabilities())
-local servers = { "ansiblels", "bashls", "dockerls", "zk", "sqls", "rnix" }
+local servers = { "ansiblels", "bashls", "dockerls", "zk", "sqls", "rnix", "terraformls" }
 for _, lsp in pairs(servers) do
 	require("lspconfig")[lsp].setup({
 		on_attach = on_attach,
