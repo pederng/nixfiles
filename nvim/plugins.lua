fa5ecdf12fc19997720fbc3d4c64b2a90a01a939
Peder Notto Galteland <peder.galteland@softwarelab.no>
2022-10-21 07:58:26 +0200

    nvim lsp fix

--- nvim/plugins.lua
+++ nvim/plugins.lua
@@ -142,7 +142,7 @@ local on_attach = function(_, bufnr)
 	require("lsp_signature").on_attach()
 end
 
-local capabilities = require("cmp_nvim_lsp").update_capabilities(vim.lsp.protocol.make_client_capabilities())
+local capabilities = require("cmp_nvim_lsp").default_capabilities(vim.lsp.protocol.make_client_capabilities())
 local servers = { "ansiblels", "bashls", "dockerls", "zk", "sqls", "rnix", "terraformls" }
 for _, lsp in pairs(servers) do
 	require("lspconfig")[lsp].setup({
