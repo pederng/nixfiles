3b65f2f3c2e5d5b6ca79ca1c0908c76cff7e7363
Peder Notto Galteland <peder.galteland@softwarelab.no>
2022-10-27 18:48:03 +0200

    add terraform_fmt

--- nvim/plugins.lua
+++ nvim/plugins.lua
@@ -289,6 +289,7 @@ local sources = {
 	null_ls.builtins.formatting.cue_fmt,
 	null_ls.builtins.formatting.stylua,
 	null_ls.builtins.formatting.alejandra,
+	null_ls.builtins.formatting.terraform_fmt,
 	null_ls.builtins.code_actions.gitsigns,
 }
 
