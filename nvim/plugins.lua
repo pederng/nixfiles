8c14e5b8218f6ade0e2c02c3c378cde5d2000f88
Peder Notto Galteland <peder.galteland@softwarelab.no>
2023-05-10 12:52:12 +0200

    Enable ruff lsp, disable mypy/pylint

--- nvim/plugins.lua
+++ nvim/plugins.lua
@@ -142,7 +142,7 @@ local on_attach = function(_, bufnr)
 end
 
 local capabilities = require("cmp_nvim_lsp").default_capabilities(vim.lsp.protocol.make_client_capabilities())
-local servers = { "ansiblels", "bashls", "dockerls", "zk", "rnix", "terraformls" }
+local servers = { "ansiblels", "bashls", "dockerls", "zk", "ruff_lsp", "rnix", "terraformls" }
 for _, lsp in pairs(servers) do
 	require("lspconfig")[lsp].setup({
 		on_attach = on_attach,
@@ -274,8 +274,8 @@ end
 
 local null_ls = require("null-ls")
 local sources = {
-	null_ls.builtins.diagnostics.mypy,
-	null_ls.builtins.diagnostics.pylint,
+	-- null_ls.builtins.diagnostics.mypy,
+	-- null_ls.builtins.diagnostics.pylint,
 	null_ls.builtins.diagnostics.hadolint,
 	null_ls.builtins.diagnostics.ansiblelint,
 	null_ls.builtins.diagnostics.shellcheck,
