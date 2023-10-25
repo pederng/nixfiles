051aa9b60bbebc99f3d552cd4777dadd8939a7f6
Peder Notto Galteland <peder.galteland@softwarelab.no>
2023-10-25 15:00:56 +0200

    Move neovim language-specific files

--- home.nix
+++ home.nix
@@ -215,8 +215,9 @@ in {
     #   (pluginGit "pedernot/vim-textobj-python")
     # ];
   };
-  home.file.".config/nvim/after/syntax/python.vim".source = ./python.vim;
-  home.file.".config/nvim/after/syntax/markdown.vim".source = ./markdown.vim;
+  home.file.".config/nvim/after/syntax/python.vim".source = ./nvim/python.vim;
+  home.file.".config/nvim/after/syntax/markdown.vim".source = ./nvim/markdown.vim;
+  home.file.".config/nvim/queries/python/injections.scm".source = ./nvim/injections.scm;
 
   programs.tmux = {
     enable = true;
