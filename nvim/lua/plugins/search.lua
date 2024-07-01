24620aeb90de52acaf569bd29a8b37c3268dbf65
Peder Notto Galteland <peder.galteland@softwarelab.no>
2024-07-01 07:37:12 +0200

    Neovim tweaking

--- nvim/lua/plugins/search.lua
+++ nvim/lua/plugins/search.lua
@@ -1,7 +1,13 @@
+local keymap = vim.keymap.set
+local silent = { silent = true }
+
 return {
 	{
 		"nvim-telescope/telescope.nvim",
-		dependencies = { "nvim-lua/plenary.nvim" },
+		dependencies = {
+			"nvim-lua/plenary.nvim",
+			"debugloop/telescope-undo.nvim",
+		},
 		config = function()
 			require("telescope").setup({
 				defaults = {
@@ -14,6 +20,18 @@ return {
 					},
 				},
 			})
+			require("telescope").load_extension("undo")
+			keymap("n", "<C-p>", ":Telescope<cr>", silent)
+			keymap("n", "<C-p><C-p>", ":Telescope<cr>", silent)
+			keymap("n", "<C-p><C-t>", ":Telescope lsp_dynamic_workspace_symbols<cr>", silent)
+			keymap("n", "<C-p><C-o>", ":Telescope lsp_document_symbols<cr>", silent)
+			keymap("n", "r<C-]>", ":Telescope lsp_references<CR>", silent)
+			keymap("n", "<C-p><C-f>", ":Telescope find_files<cr>", silent)
+			keymap("n", "<C-p><C-g>", ":Telescope live_grep<cr>", silent)
+			keymap("n", "<C-p><C-h>", ":Telescope oldfiles<cr>", silent)
+			keymap("n", "<C-p><C-b>", ":Telescope buffers<cr>", silent)
+			keymap("n", "<C-p><C-c>", ":Telescope commands<cr>", silent)
+			keymap("n", "<C-p><C-u>", ":Telescope undo<cr>", silent)
 		end,
 	},
 	{
