acfa08734a5ff072a31894f444e28677c182d2fe
Peder Notto Galteland <peder.galteland@softwarelab.no>
2025-05-09 14:06:59 +0200

    Factor out terminal.nix

--- programs.nix
+++ programs.nix
@@ -13,40 +13,11 @@
   imports = [
     ./ssh.nix
     ./version_control.nix
+    ./terminal.nix
   ];
 
   programs = {
     home-manager.enable = true;
-
-    zsh = {
-      enable = true;
-      dotDir = ".config/zsh";
-      profileExtra = lib.strings.fileContents ./zsh/zprofile;
-      plugins = [
-        {
-          name = "zsh-nix-shell";
-          file = "nix-shell.plugin.zsh";
-          src = pkgs.fetchFromGitHub {
-            owner = "chisui";
-            repo = "zsh-nix-shell";
-            rev = "v0.8.0";
-            sha256 = "1lzrn0n4fxfcgg65v0qhnj7wnybybqzs4adz7xsrkgmcsr0ii8b7";
-          };
-        }
-      ];
-      syntaxHighlighting.enable = true;
-      initContent = builtins.concatStringsSep "\n" [
-        ''
-          ${lib.strings.fileContents ./zsh/zshrc}
-          ${lib.strings.fileContents ./zsh/aliases.zsh}
-          ${lib.strings.fileContents ./zsh/functions.zsh}
-        ''
-      ];
-      history = {
-        path = "$ZDOTDIR/zsh/histfile";
-      };
-      defaultKeymap = "viins";
-    };
 
     password-store = {
       enable = true;
@@ -54,7 +25,6 @@
         PASSWORD_STORE_DIR = "${config.xdg.dataHome}/pass";
       };
     };
-
 
     gpg = {
       enable = true;
@@ -74,61 +44,6 @@
         tree-sitter
         lua-language-server
       ];
-    };
-
-    foot = {
-      enable = true;
-      settings = {
-        main = {
-          dpi-aware = "yes";
-          font = "Hack Nerd Font Mono:size=8, Noto Color Emoji:size=8";
-          include = "~/.local/share/tinted-theming/tinty/tinted-terminal-themes-foot-file.ini";
-        };
-      };
-
-    };
-
-    tmux = {
-      enable = true;
-      package = pkgs.tmux;
-      extraConfig = builtins.concatStringsSep "\n" [
-        (lib.strings.fileContents ./tmux.conf)
-      ];
-      plugins = with pkgs; [
-        {
-          plugin = tmuxPlugins.tmux-thumbs;
-          extraConfig = ''
-            set -g @thumbs-alphabet qwerty-homerow
-            set -g @thumbs-bg-color blue
-            set -g @thumbs-fg-color green
-            set -g @thumbs-hint-bg-color black
-            set -g @thumbs-hint-fg-color yellow
-          '';
-        }
-        {
-          plugin = tmuxPlugins.tmux-fzf;
-          extraConfig = ''
-            bind w choose-tree -Z
-          '';
-        }
-      ];
-    };
-
-    zoxide = {
-      enable = true;
-      enableZshIntegration = true;
-    };
-
-    fzf = {
-      enable = true;
-      enableZshIntegration = true;
-      defaultCommand = "fd --type d";
-    };
-
-    direnv = {
-      enable = true;
-      enableZshIntegration = true;
-      nix-direnv.enable = true;
     };
 
     newsboat = {
@@ -207,35 +122,5 @@
       };
     };
 
-    starship = {
-      enable = true;
-      package = pkgs.starship;
-      settings = {
-        python = {symbol = " ";};
-        rust = {symbol = " ";};
-        directory = {fish_style_pwd_dir_length = 1;};
-        command_timeout = 1000;
-        kubernetes = {
-          disabled = false;
-          format = "[$context](blue)[\(:$namespace\)](dimmed green) ";
-          contexts = [
-            {
-              context_pattern = "kind-coffer-dev";
-              context_alias = "dev";
-            }
-            {
-              context_pattern = "coffer-test-aks";
-              context_alias = "test";
-            }
-            {
-              context_pattern = "coffer-prod-dora";
-              context_alias = "prod";
-            }
-          ];
-        };
-
-        aws = {disabled = true;};
-      };
-    };
   };
 }
---
+++ terminal.nix
@@ -0,0 +1,118 @@
+{pkgs, lib, ...}:{
+  programs = {
+    foot = {
+      enable = true;
+      settings = {
+        main = {
+          dpi-aware = "yes";
+          font = "Hack Nerd Font Mono:size=8, Noto Color Emoji:size=8";
+          include = "~/.local/share/tinted-theming/tinty/tinted-terminal-themes-foot-file.ini";
+        };
+      };
+    };
+
+    zsh = {
+      enable = true;
+      dotDir = ".config/zsh";
+      profileExtra = lib.strings.fileContents ./zsh/zprofile;
+      plugins = [
+        {
+          name = "zsh-nix-shell";
+          file = "nix-shell.plugin.zsh";
+          src = pkgs.fetchFromGitHub {
+            owner = "chisui";
+            repo = "zsh-nix-shell";
+            rev = "v0.8.0";
+            sha256 = "1lzrn0n4fxfcgg65v0qhnj7wnybybqzs4adz7xsrkgmcsr0ii8b7";
+          };
+        }
+      ];
+      syntaxHighlighting.enable = true;
+      initContent = builtins.concatStringsSep "\n" [
+        ''
+          ${lib.strings.fileContents ./zsh/zshrc}
+          ${lib.strings.fileContents ./zsh/aliases.zsh}
+          ${lib.strings.fileContents ./zsh/functions.zsh}
+        ''
+      ];
+      history = {
+        path = "$ZDOTDIR/zsh/histfile";
+      };
+      defaultKeymap = "viins";
+    };
+
+    tmux = {
+      enable = true;
+      package = pkgs.tmux;
+      extraConfig = builtins.concatStringsSep "\n" [
+        (lib.strings.fileContents ./tmux.conf)
+      ];
+      plugins = with pkgs; [
+        {
+          plugin = tmuxPlugins.tmux-thumbs;
+          extraConfig = ''
+            set -g @thumbs-alphabet qwerty-homerow
+            set -g @thumbs-bg-color blue
+            set -g @thumbs-fg-color green
+            set -g @thumbs-hint-bg-color black
+            set -g @thumbs-hint-fg-color yellow
+          '';
+        }
+        {
+          plugin = tmuxPlugins.tmux-fzf;
+          extraConfig = ''
+            bind w choose-tree -Z
+          '';
+        }
+      ];
+    };
+
+    zoxide = {
+      enable = true;
+      enableZshIntegration = true;
+    };
+
+    fzf = {
+      enable = true;
+      enableZshIntegration = true;
+      defaultCommand = "fd --type d";
+    };
+
+    direnv = {
+      enable = true;
+      enableZshIntegration = true;
+      nix-direnv.enable = true;
+    };
+
+    starship = {
+      enable = true;
+      package = pkgs.starship;
+      settings = {
+        python = {symbol = " ";};
+        rust = {symbol = " ";};
+        directory = {fish_style_pwd_dir_length = 1;};
+        command_timeout = 1000;
+        kubernetes = {
+          disabled = false;
+          format = "[$context](blue)[\(:$namespace\)](dimmed green) ";
+          contexts = [
+            {
+              context_pattern = "kind-coffer-dev";
+              context_alias = "dev";
+            }
+            {
+              context_pattern = "coffer-test-aks";
+              context_alias = "test";
+            }
+            {
+              context_pattern = "coffer-prod-dora";
+              context_alias = "prod";
+            }
+          ];
+        };
+        aws = {disabled = true;};
+      };
+    };
+
+  };
+}
