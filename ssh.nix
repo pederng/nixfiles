d385c2d0011a948683e33d019cc1f903eb4fb717
Peder Notto Galteland <peder.galteland@softwarelab.no>
2025-05-23 09:21:40 +0200

    Add dora-cluster and ci-cluster with forwarding

--- ssh.nix
+++ ssh.nix
@@ -29,6 +29,18 @@ _:{
       "search*" = {
         proxyCommand = "ssh -q -W %h:%p bast1";
       };
+      "dora-cluster" = {
+        hostname = "search1";
+        dynamicForwards = [ { port = 1080; }];
+        proxyCommand = "ssh -q -W search1:%p bast1";
+        extraOptions = { SessionType = "none"; };
+      };
+      "ci-cluster" = {
+        hostname = "bk-ci3";
+        dynamicForwards = [ { port = 1080; }];
+        proxyCommand = "ssh -q -W bk-ci3.dhcp.bk.cptr.no:%p asbest";
+        extraOptions = { SessionType = "none"; };
+      };
       "bk-ci**" = {
         proxyCommand = "ssh -q -W %h.dhcp.bk.cptr.no:%p asbest";
       };
