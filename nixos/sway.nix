eed6b7da825b598da259e590b7afa70a852ab230
Peder Notto Galteland <peder.galteland@softwarelab.no>
2025-07-15 21:26:36 +0200

    Add touchpad config for personal laptop

---
+++ nixos/sway.nix
@@ -0,0 +1,13 @@
+_: {
+  wayland.windowManager.sway = {
+    config = {
+      input = {
+        "2:7:SynPS/2_Synaptics_TouchPad" = {
+          dwt = "enabled";
+          tap = "enabled";
+          middle_emulation = "enabled";
+        };
+      };
+    };
+  };
+}
