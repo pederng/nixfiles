40c30fd4280af39984a95be23edb69b69d09a077
Peder Notto Galteland <peder.galteland@softwarelab.no>
2025-07-15 21:39:04 +0200

    Add hledger on lapping

--- nixos/home.nix
+++ nixos/home.nix
@@ -1,6 +1,12 @@
-{...}: {
+{pkgs, ...}: {
   imports = [
     ../common
     ./sway.nix
   ];
+  home.packages = with pkgs; [
+    hledger
+  ];
+  home.sessionVariables = {
+    LEGDER_FILE = "$HOME/workspace/finances/main.journal";
+  };
 }
